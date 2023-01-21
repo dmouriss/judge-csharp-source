9ab1526b87314423bc4e11d245a717c508f2b036
dmouriss <dieter.mourisse@howest.be>
2023-01-21 14:04:36 +0100

    Set testcase status to false when there is a failed empty test

---
+++ judge-csharp/EventHandling/OutputHandler.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using judge_csharp.Feedback;
+using System.Xml;
+using System.IO;
+using judge_csharp.Json;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace judge_csharp.EventHandling
+{
+    public class OutputHandler
+    {
+        private readonly TextWriter OutputChannel;
+        private readonly Dictionary<string, Status> resultmap;
+
+        public OutputHandler(TextWriter outputChannel)
+        {
+            OutputChannel = outputChannel;
+            resultmap = new Dictionary<string, Status>();
+            resultmap.Add("Passed", Status.CORRECT);
+            resultmap.Add("Failed", Status.WRONG);
+        }
+
+        public bool Handle(string report)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(report);
+
+            XmlNode node = doc.FirstChild;
+            string nodetype = node.Name;
+
+            bool proceed = true;
+
+            switch (nodetype)
+            {
+                case "start-run":
+                    OutputChannel.WriteLine(JSONGenerator.StartJudgement());
+                    break;
+                case "start-suite":
+                    if (node.Attributes["type"].Value == "TestFixture")
+                    {
+                        string tabName = node.Attributes["name"].Value;
+                        OutputChannel.WriteLine(JSONGenerator.StartTab(tabName));
+                    }
+                    break;
+                case "start-test":
+                    OutputChannel.WriteLine(JSONGenerator.StartContext());
+
+                    string testcaseDescription = node.Attributes["name"].Value;
+
+                    if (node.SelectNodes("stdin").Count == 0)
+                        OutputChannel.WriteLine(JSONGenerator.StartTestcase(testcaseDescription));
+                    else
+                        OutputChannel.WriteLine(JSONGenerator.StartTestcase(node.SelectSingleNode("stdin").ChildNodes[0].Value.Trim()));
+                    break;
+                case "test-case":
+                    if (node.SelectNodes("assertions").Count == 0) /* runtime error */
+                    {
+                        string messagetext = node.SelectSingleNode("failure/message").ChildNodes[0].Value;
+                        OutputChannel.WriteLine(JSONGenerator.AppendMessage(Format.PLAIN, messagetext));
+                        OutputChannel.WriteLine(JSONGenerator.EscalateStatus(Status.RUNTIME_ERROR));
+                        OutputChannel.WriteLine(JSONGenerator.CloseTestcase(false));
+                    }
+                    else
+                    {
+                        bool? accepted = null;
+                        foreach (XmlNode assertion in node.SelectSingleNode("assertions"))
+                        {
+                            string testresult = assertion.Attributes["result"].Value;
+                            string expected = "";
+                            string generated = "";
+                            string message = "";
+                            if (assertion.SelectNodes("actual").Count != 0 && assertion.SelectNodes("expected").Count != 0)
+                            {
+                                expected = assertion.SelectSingleNode("expected").ChildNodes[0].Value.Trim();
+                                generated = assertion.SelectSingleNode("actual").ChildNodes[0].Value.Trim();
+                            }
+
+                            if (assertion.SelectNodes("message").Count != 0)
+                                message = assertion.SelectSingleNode("message").ChildNodes[0].Value.Trim();
+
+                            if (generated != "" || expected != "" || message != "")
+                            {
+                                OutputChannel.WriteLine(JSONGenerator.StartTest(expected));
+                                if (message.Length != 0)
+                                    OutputChannel.WriteLine(JSONGenerator.AppendMessage(Format.PLAIN, message));
+                                OutputChannel.WriteLine(JSONGenerator.CloseTest(resultmap[testresult], generated));
+                            }
+                            else if (resultmap[testresult] != Status.CORRECT)
+                            {
+                                accepted = false;
+                            }
+                        }
+                        OutputChannel.WriteLine(JSONGenerator.CloseTestcase(accepted));
+                    }
+                    OutputChannel.WriteLine(JSONGenerator.CloseContext());
+                    break;
+                case "test-suite":
+                    if (node.Attributes["type"].Value == "TestFixture")
+                        OutputChannel.WriteLine(JSONGenerator.CloseTab());
+                    break;
+                case "test-run":
+                    OutputChannel.WriteLine(JSONGenerator.CloseJudgement());
+                    proceed = false;
+                    break;
+            }
+            return proceed;
+        }
+    }
+}
---
+++ judge-csharp/Json/JSONGenerator.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace judge_csharp.Json
+{
+    public static class JSONGenerator
+    {
+
+        private static Dictionary<string, object> GetCommandDict(string command)
+        {
+
+            Dictionary<string, object> content = new Dictionary<string, object>
+            {
+                { "command", command}
+            };
+
+            return content;
+        }
+
+        public static string StartJudgement()
+        {
+            Dictionary<string, object> content = GetCommandDict("start-judgement");
+            return JsonConvert.SerializeObject(content);
+        }
+
+        public static string CloseJudgement()
+        {
+            Dictionary<string, object> content = GetCommandDict("close-judgement");
+            return JsonConvert.SerializeObject(content);
+        }
+
+        public static string StartTab(string name)
+        {
+
+            Dictionary<string, object> content = GetCommandDict("start-tab");
+            content.Add("title", name);
+            return JsonConvert.SerializeObject(content);
+        }
+
+        public static string CloseTab()
+        {
+            Dictionary<string, object> content = GetCommandDict("close-tab");
+            return JsonConvert.SerializeObject(content);
+        }
+
+        public static string StartContext()
+        {
+            Dictionary<string, object> content = GetCommandDict("start-context");
+            return JsonConvert.SerializeObject(content);
+        }
+
+        public static string CloseContext()
+        {
+            Dictionary<string, object> content = GetCommandDict("close-context");
+            return JsonConvert.SerializeObject(content);
+        }
+
+        public static string StartTestcase(string description)
+        {
+            Dictionary<string, object> content = GetCommandDict("start-testcase");
+            Dictionary<string, object> descriptionobject = new Dictionary<string, object>
+            {
+                { "format", "html" },
+                { "description", "<span class=\"code\">" + description + "</span>" }
+            };
+            content.Add("description", descriptionobject);
+            return JsonConvert.SerializeObject(content);
+        }
+
+        public static string CloseTestcase(bool? accepted = null)
+        {
+            Dictionary<string, object> content = GetCommandDict("close-testcase");
+            if (accepted != null)
+                content.Add("accepted", accepted);
+            return JsonConvert.SerializeObject(content);
+        }
+
+        public static string StartTest(string expected)
+        {
+            Dictionary<string, object> content = GetCommandDict("start-test");
+            content.Add("expected", expected);
+            return JsonConvert.SerializeObject(content);
+        }
+
+        public static string CloseTest(Feedback.Status status, string generated)
+        {
+            Dictionary<string, object> content = GetCommandDict("close-test");
+
+            Dictionary<string, object> statusEnum = new Dictionary<string, object>
+            {
+                { "enum", status }
+            };
+
+            content.Add("status", statusEnum);
+            content.Add("generated", generated);
+            return JsonConvert.SerializeObject(content);
+        }
+
+        public static string AppendMessage(Feedback.Format format, string description)
+        {
+            Dictionary<string, object> content = GetCommandDict("append-message");
+            Dictionary<string, object> message = new Dictionary<string, object>
+            {
+                { "format", format },
+                { "description", description }
+            };
+
+            content.Add("message", message);
+            return JsonConvert.SerializeObject(content);
+        }
+
+        public static string EscalateStatus(Feedback.Status status)
+        {
+            Dictionary<string, object> content = GetCommandDict("escalate-status");
+            Dictionary<string, object> statusEnum = new Dictionary<string, object>
+            {
+                { "enum", status }
+            };
+
+            content.Add("status", statusEnum);
+            return JsonConvert.SerializeObject(content);
+        }
+    }
+}
